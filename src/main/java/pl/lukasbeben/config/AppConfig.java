8650ae05e08186482fb1842bb7bfa67f6c19f77f
LukBeben <lukas.beben@gmail.com>
2022-08-01 13:07:22 +0200

    Konfiguracja pod Spring zakończona pomyślnie

---
+++ src/main/java/pl/lukasbeben/config/AppConfig.java
@@ -0,0 +1,23 @@
+package pl.lukasbeben.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.lukasbeben")
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
